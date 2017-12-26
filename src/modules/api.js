977741545ed89fc7ad44214bdcc6005a0c4808b9
livingcreative <dnkreative@gmail.com>
2017-12-26 22:27:03 +0500

    api base removed, using current host and webpack dev server proxy

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,4 +1,3 @@
-export const API_BASE = "http://localhost:3000"
 export const API_LOGIN = "/chat/login"
 export const API_LOGIN_ATTEMPT = "/chat/login/attempt"
 export const API_LOGOUT = "/chat/logout"
@@ -6,6 +5,5 @@ export const API_LOGOUT = "/chat/logout"
 import axios from 'axios'
 
 export const API = axios.create({
-    baseURL: API_BASE,
     withCredentials: true
 })
